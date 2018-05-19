63184a7af5eab1b6bf0580eb885ee38b458ab65e
Bartosz Walkowicz <wallko1@hotmail.com>
2018-05-19 17:07:32 +0200

    add additional logging and minor improvements

--- server/andromover_mouse.py
+++ server/andromover_mouse.py
@@ -1,22 +1,36 @@
+import sys
 import json
 from typing import Union, Optional
 
 import pyautogui as pg
 
 
-class Mouse:
+EVENTS = {}
 
-    def __call__(self, msg: Union[bytes, bytearray]):
-        event = json.loads(msg.decode())
-        try:
-            method = getattr(self, event['type'])
-            method(**event['details'])
-        except KeyError:
-            pass
 
-    def click(self, *, x: Optional[float] = None, y: Optional[float] = None,
-              button: str):
-        pg.click(x=x, y=y, button=button)
+def event(fun):
+    EVENTS[fun.__name__] = fun
 
-    def move(self, x: float, y: float):
-        pg.moveRel(x, y)
+
+def mouse(msg: Union[bytes, bytearray]):
+    print('Received msg: ', msg)
+
+    try:
+        decoded_msg = msg.decode()
+        mouse_event = json.loads(decoded_msg)
+        event_type = mouse_event['type']
+        event_details = mouse_event['details']
+        fun = EVENTS[event_type]
+        fun(**event_details)
+    except Exception as ex:
+        print('Failed to handle mouse event: ', ex, file=sys.stderr)
+
+
+@event
+def click(button: str, x: Optional[float] = None, y: Optional[float] = None):
+    pg.click(button=button, x=x, y=y)
+
+
+@event
+def move(x: float, y: float):
+    pg.moveRel(x, y, 0.1)
--- server/andromover_server.py
+++ server/andromover_server.py
@@ -2,11 +2,12 @@ import socket
 import argparse
 from typing import Callable, Optional
 
-from andromover_mouse import Mouse
+from andromover_mouse import mouse
 
 
 BACKLOG = 0
-BLUETOOTH_PROTO = socket.BTPROTO_RFCOMM
+PACKET_PREAMBLE = 4
+RECV_SIZE = 1024
 
 
 def echo(msg):
@@ -14,8 +15,18 @@ def echo(msg):
     return msg.upper()
 
 
-def recv(sock: socket.socket, size: Optional[int] = 4):
-    msg_size = int(sock.recv(size))
+def recv(sock: socket.socket):
+    preamble_size = PACKET_PREAMBLE
+    preamble = b''
+    while preamble_size:
+        preamble_part = sock.recv(preamble_size)
+        if preamble_part:
+            preamble += preamble_part
+            preamble_size -= len(preamble_part)
+        else:
+            return preamble_part
+
+    msg_size = int.from_bytes(preamble, byteorder='big')
     buffer = bytearray(msg_size)
     mv = memoryview(buffer)
     while msg_size:
@@ -25,37 +36,45 @@ def recv(sock: socket.socket, size: Optional[int] = 4):
     return buffer
 
 
-def serve(address: str, port: int, *,
-          bluetooth: bool = False, keepalive: Optional[float] = 30,
+def serve(address: str, port: int, *, bluetooth: bool = False,
+          keepalive: Optional[float] = 30, preamble: bool = False,
           callback: Callable[[bytes], Optional[bytes]] = echo):
 
     if bluetooth:
         server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
-                                    BLUETOOTH_PROTO)
+                                    socket.BTPROTO_RFCOMM)
     else:
         server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     server_sock.bind((address, port))
     server_sock.listen(BACKLOG)
 
-    with server_sock:
-        while True:
-            client_sock, client_addr = server_sock.accept()
-            client_sock.settimeout(keepalive)
-            print('Started connection with: {}'.format(client_addr))
-            try:
-                while True:
-                    data = client_sock.recv(1024)
-                    if not data:
-                        print('Connection closed by peer')
-                        break
-                    reply = callback(data)
-                    if reply:
-                        client_sock.send(reply)
-            except socket.timeout:
-                print('Connection timed out')
-            finally:
-                client_sock.close()
+    recv_fun = recv if preamble else lambda sock: sock.recv(RECV_SIZE)
+
+    try:
+        with server_sock:
+            while True:
+                client_sock, client_addr = server_sock.accept()
+                client_sock.settimeout(keepalive)
+                print('Started connection with: {}'.format(client_addr))
+                try:
+                    while True:
+                        data = recv_fun(client_sock)
+                        if not data:
+                            print('Connection closed by peer')
+                            break
+                        reply = callback(data)
+                        if reply:
+                            client_sock.send(reply)
+                except socket.timeout:
+                    print('Connection timed out')
+                finally:
+                    client_sock.close()
+    except KeyboardInterrupt:
+        print('Server shutdown')
+    finally:
+        server_sock.close()
 
 
 if __name__ == '__main__':
@@ -71,7 +90,11 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--keepalive', type=float, default=None,
                         help='Timeout of inactivity on connection after which '
                              'it is severed (by default it is infinity)')
+    parser.add_argument('--preamble', action='store_true',
+                        help='If specified, the first 4 bytes of every msg are'
+                             'considered to be the length of the rest of the msg')
     args = parser.parse_args()
 
     serve(args.address, args.port, bluetooth=args.bluetooth,
-          keepalive=args.keepalive, callback=Mouse())
+          keepalive=args.keepalive, preamble=args.preamble,
+          callback=mouse)
